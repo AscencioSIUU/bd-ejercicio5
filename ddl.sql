d465d45ceee50c3b45ad32f98af0675740ac1e80
Nesstor07 <erneram1975@gmail.com>
2025-04-08 10:25:18 -0600

    ddl done

---
+++ ddl.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE pet_shop;
+CREATE TABLE "specie" (
+  "id" serial PRIMARY KEY,
+  "name" varchar(255)
+);
+
+CREATE TABLE "specie_races" (
+  "id" serial PRIMARY KEY,
+  "specie_id" int REFERENCES "specie" ("id"),
+  "name" varchar(255)
+);
+
+CREATE TABLE "owner" (
+  "id" serial PRIMARY KEY,
+  "name" varchar(255),
+  "phone" varchar(15),
+  "address" text
+);
+
+CREATE TABLE "pet" (
+  "id" serial PRIMARY KEY,
+  "spcie_race_id" int REFERENCES "specie_races" ("id"),
+  "owner_id" int REFERENCES "owner" ("id"),
+  "weight" numeric(10,2),
+  "birth_date" date,
+  "isAlive" boolean
+);
+
+CREATE TABLE "vaccine" (
+  "id" serial PRIMARY KEY,
+  "name" varchar(255)
+);
+
+CREATE TABLE "pet_vaccines" (
+  "id" serial PRIMARY KEY,
+  "pet_id" int REFERENCES "pet" ("id"),
+  "vaccine_id" int REFERENCES "vaccine" ("id")
+);
+
+CREATE TABLE "medical_treatment" (
+  "id" serial PRIMARY KEY,
+  "pet_id" int REFERENCES "pet" ("id"),
+  "description" text
+);
+
+CREATE TABLE "category" (
+  "id" serial PRIMARY KEY,
+  "name" varchar(255)
+);
+
+CREATE TABLE "product" (
+  "id" serial PRIMARY KEY,
+  "category_id" int REFERENCES "category" ("id"),
+  "name" varchar(255),
+  "brand" varchar(255),
+  "current_stock" int
+);
+
+CREATE TABLE "inventory_logs" (
+  "id" serial PRIMARY KEY,
+  "quantity" int,
+  "product_id" int REFERENCES "product" ("id")
+);
+
+CREATE TABLE "appointment" (
+  "id" serial PRIMARY KEY,
+  "pet_id" int REFERENCES "pet" ("id"),
+  "owner_id" int REFERENCES "owner" ("id"),
+  "date" timestamp
+);
+
+CREATE TABLE "sale" (
+  "id" serial PRIMARY KEY,
+  "owner_id" int REFERENCES "owner" ("id"),
+  "date" timestamp
+);
+
+CREATE TABLE "sale_details" (
+  "id" serial PRIMARY KEY,
+  "sale_id" int REFERENCES "sale" ("id"),
+  "product_id" int REFERENCES "product" ("id"),
+  "quantity" int
+);
